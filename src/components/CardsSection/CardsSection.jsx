e68cd2b2f9cb0dfd4908793127c7c9128c766f95
Hurin Oleksandr <gurinalex91@gmail.com>
2022-11-15 21:32:21 +0200

    fix cards bg

--- src/components/CardsSection/CardsSection.jsx
+++ src/components/CardsSection/CardsSection.jsx
@@ -5,11 +5,9 @@ import {
   Card,
   CardTitle,
   CardLink,
-  GradientLayer,
 } from "./CardsSection.styles";
 
 import { Container, SectionWrap, Title } from "../../GlobalStyles";
-import CardBG1 from "../../images/bg_card1.png";
 
 const CardsSection = () => {
   return (
@@ -17,8 +15,6 @@ const CardsSection = () => {
       <Container>
         <CardsWraper>
           <Card className="first" href="#">
-            <img src={CardBG1} alt="" />
-            <GradientLayer>
               <CardTitle>Spot & Margin</CardTitle>
               <Title>
                 Trade <span className="colored">200+ </span> <br />
@@ -26,17 +22,13 @@ const CardsSection = () => {
                 <br /> to 10x leverage
               </Title>
               <CardLink href="#">Learn more</CardLink>
-            </GradientLayer>
           </Card>
           <Card href="#">
-            <GradientLayer>
               <CardTitle>Derivatives</CardTitle>
               <Title>40+ quarterly futures and contracts</Title>
               <CardLink href="#">Learn more</CardLink>
-            </GradientLayer>
           </Card>
           <Card href="#">
-            <GradientLayer>
               <CardTitle>Trading Arena</CardTitle>
               <Title>
                 Prize pools
@@ -45,17 +37,14 @@ const CardsSection = () => {
                 <br /> USD 1,000,000
               </Title>
               <CardLink href="#">Learn more</CardLink>
-            </GradientLayer>
           </Card>
           <Card className="last" href="#">
-            <GradientLayer>
               <CardTitle>mobile app</CardTitle>
               <Title>
                 Trade anytime, <br />
                 anywhere
               </Title>
               <CardLink href="#">Learn more</CardLink>
-            </GradientLayer>
           </Card>
         </CardsWraper>
       </Container>
--- src/components/CardsSection/CardsSection.styles.js
+++ src/components/CardsSection/CardsSection.styles.js
@@ -1,38 +1,14 @@
 import styled from "styled-components";
 
+import CardBG1 from "../../images/bg_card1.png";
 import CardBG2 from "../../images/bg_card2.png";
 
 export const CardsWraper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 30px;
-`;
-
-export const GradientLayer = styled.div`
-  height: 100%;
-  padding: 30px;
-  display: flex;
-  flex-direction: column;
-  gap: 45px;
-  background: radial-gradient(
-        ellipse 180px 90px at top left,
-        rgba(255, 255, 255, 0.1) 0%,
-        rgba(196, 196, 196, 0) 100%
-      )
-      left 0 top 0,
-    radial-gradient(
-        ellipse 180px 90px at right bottom,
-        rgba(255, 255, 255, 0.1) 0%,
-        rgba(196, 196, 196, 0) 100%
-      )
-      bottom 50px right 0,
-    radial-gradient(
-        circle at bottom right,
-        rgba(255, 255, 255, 0.07) 0%,
-        rgba(196, 196, 196, 0) 100%
-      )
-      bottom 50px right 0;
-  background-repeat: no-repeat;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 30px;
+  }
 `;
 
 export const CardLink = styled.a`
@@ -45,56 +21,83 @@ export const CardLink = styled.a`
 `;
 
 export const Card = styled.a`
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 45px;
   border: 2px solid rgba(211, 211, 211, 0.35);
   border-radius: 46px;
   overflow: hidden;
   background-color: #000;
   transition: all 0.2s ease-out;
+  background: radial-gradient(
+        ellipse 180px 90px at top left,
+        rgba(255, 255, 255, 0.1) 0%,
+        rgba(196, 196, 196, 0) 100%
+      )
+      left top,
+    radial-gradient(
+        ellipse 180px 90px at right bottom,
+        rgba(255, 255, 255, 0.1) 0%,
+        rgba(196, 196, 196, 0) 100%
+      )
+      bottom right,
+    radial-gradient(
+        circle at bottom right,
+        rgba(255, 255, 255, 0.07) 0%,
+        rgba(196, 196, 196, 0) 100%
+      )
+      bottom right;
+  background-repeat: no-repeat;
   &.first {
-    grid-row-start: 1;
-    grid-row-end: 3;
-    ${GradientLayer} {
-      background: radial-gradient(
-            ellipse 260px 170px at top left,
-            rgba(255, 255, 255, 0.1) 0%,
-            rgba(196, 196, 196, 0) 100%
-          )
-          left top,
-        radial-gradient(
-            ellipse 260px 170px at right bottom,
-            rgba(255, 255, 255, 0.1) 0%,
-            rgba(196, 196, 196, 0) 100%
-          )
-          bottom 350px right;
-      background-repeat: no-repeat;
-      padding-top: 350px;
-      margin-top: -510px;
-    }
+    display: flex;
+    flex-direction: column;
+    background: url(${CardBG1}) top,
+      radial-gradient(
+          ellipse 260px 170px at top left,
+          rgba(255, 255, 255, 0.1) 0%,
+          rgba(196, 196, 196, 0) 100%
+        )
+        left top,
+      radial-gradient(
+          ellipse 260px 170px at right bottom,
+          rgba(255, 255, 255, 0.1) 0%,
+          rgba(196, 196, 196, 0) 100%
+        )
+        bottom right;
+    background-repeat: no-repeat;
+    padding-top: 350px;
   }
   &.last {
-    grid-column-start: 2;
-    grid-column-end: 4;
-    background: url(${CardBG2}) no-repeat right top;
-    ${GradientLayer} {
-      background: radial-gradient(
-            ellipse 375px 75px at top,
-            rgba(255, 255, 255, 0.1) 0%,
-            rgba(196, 196, 196, 0) 100%
-          )
-          left -15rem top 0,
-        radial-gradient(
-            ellipse 375px 75px at bottom,
-            rgba(255, 255, 255, 0.1) 0%,
-            rgba(196, 196, 196, 0) 100%
-          )
-          right -15rem bottom 50px,
-        radial-gradient(
-            ellipse 210px 175px at bottom right,
-            rgba(255, 255, 255, 0.1) 0%,
-            rgba(196, 196, 196, 0) 100%
-          )
-          bottom 60px right 0;
-      background-repeat: no-repeat;
+    background: url(${CardBG2}) right top,
+      radial-gradient(
+          ellipse 375px 75px at top,
+          rgba(255, 255, 255, 0.1) 0%,
+          rgba(196, 196, 196, 0) 100%
+        )
+        left -15rem top,
+      radial-gradient(
+          ellipse 375px 75px at bottom,
+          rgba(255, 255, 255, 0.1) 0%,
+          rgba(196, 196, 196, 0) 100%
+        )
+        right -15rem bottom,
+      radial-gradient(
+          ellipse 210px 175px at bottom right,
+          rgba(255, 255, 255, 0.1) 0%,
+          rgba(196, 196, 196, 0) 100%
+        )
+        bottom right;
+    background-repeat: no-repeat;
+  }
+  @media (min-width: 768px) {
+    &.first {
+      grid-row-start: 1;
+      grid-row-end: 3;
+    }
+    &.last {
+      grid-column-start: 2;
+      grid-column-end: 4;
     }
   }
   &:hover {
