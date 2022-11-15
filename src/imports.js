7008107a4af7b86fd5fc0563de5a9e9cd285294c
Hurin Oleksandr <gurinalex91@gmail.com>
2022-11-15 11:44:26 +0200

    add images

---
+++ src/imports.js
@@ -0,0 +1,10 @@
+import AlgoritmIMG from "./images/algoritm.png";
+import AlgoritmIMG2 from "./images/algoritm2.png";
+import { ReactComponent as LinkArrow } from "./icons/shape.svg";
+import FsBg from "./images/fs_bg.png";
+
+import { ReactComponent as MouseIco } from "./icons/mouse.svg";
+import Triangle1 from "./images/polygon_1.svg";
+import Triangle2 from "./images/polygon_2.svg";
+
+export { AlgoritmIMG, AlgoritmIMG2, FsBg, LinkArrow, MouseIco, Triangle1, Triangle2 };
